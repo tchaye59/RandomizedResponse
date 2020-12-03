d0ac5850e2205f9da8a211d5a4e868cab0963fa7
tchaye59 <tchaye59@yahoo.fr>
2020-12-03 18:29:26 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RandomizedResponse',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Jude TCHAYE',
+    author_email='tchaye59@gmail.com',
+    description='Applies a RandomizedResponse to the input. The RandomizedResponse layer randomly sets input units to random values with a frequency of rate at each step during training time, which helps prevent overfitting.'
+)
